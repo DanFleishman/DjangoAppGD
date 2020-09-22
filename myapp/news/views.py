a411135d154c3bbc7d9a241fb850ed527180d5b1
DanFleishman <63521080+DanFleishman@users.noreply.github.com>
2020-09-22 13:18:46 +0300

    Страница для создания статей (без функцианала)

--- myapp/news/views.py
+++ myapp/news/views.py
@@ -5,3 +5,6 @@ from .models import Articles
 def news_home(request):
     news = Articles.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
+
+def create(reqest):
+    return render(reqest, 'news/create.html') 
